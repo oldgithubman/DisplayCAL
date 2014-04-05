oldgithubman/DisplayCAL

commit 1d3b868b0ee553484333cda6e0ade3e5ade01814

Changed standalone curve viewer to not update the current display configuration when moved between displays. Also changed standalone profile information to query the display profile of the display the window is on.
git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@1737 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

dispcalGUI/wxMeasureFrame.py
@@ -7,7 +7,7 @@ import sys
 import config
 import localization as lang
 from config import (btn_width_correction, defaults, getcfg, geticon, 
-					get_display_number, 
+					get_argyll_display_number, get_display_number, 
 					get_display_rects, scale_adjustment_factor, setcfg,
 					writecfg)
 from debughelpers import handle_error
@@ -466,15 +466,10 @@ class MeasureFrame(InvincibleFrame):
 		if display_no != self.display_no:
 			self.display_no = display_no
 			# Translate from wx display index to Argyll display index
-			geometry = "%i, %i, %ix%i" % tuple(geometry)
-			for i, display in enumerate(getcfg("displays").split(os.pathsep)):
-				if display.find("@ " + geometry) > -1:
-					if debug:
-						safe_print("[D] Found display %s at index %i" % 
-								   (geometry, i))
-					# Save Argyll display index to configuration
-					setcfg("display.number", i + 1)
-					break
+			n = get_argyll_display_number(geometry)
+			if n is not None:
+				# Save Argyll display index to configuration
+				setcfg("display.number", i + 1)
 
 	def size_handler(self, event):
 		if debug: safe_print("[D] measureframe_size_handler")

dispcalGUI/config.py
@@ -233,6 +233,18 @@ def get_display_name(n=None, include_geometry=False):
 	return ""
 
 
+def get_argyll_display_number(geometry):
+	""" Translate from wx display geometry to Argyll display index """
+	geometry = "%i, %i, %ix%i" % tuple(geometry)
+	for i, display in enumerate(getcfg("displays").split(os.pathsep)):
+		if display.find("@ " + geometry) > -1:
+			if debug:
+				from log import safe_print
+				safe_print("[D] Found display %s at index %i" % 
+						   (geometry, i))
+			return i
+
+
 def get_display_number(display_no):
 	""" Translate from Argyll display index to wx display index """
 	from wxaddons import wx

dispcalGUI/wxLUTViewer.py
@@ -10,8 +10,9 @@ import tempfile
 from numpy import interp
 
 from argyll_cgats import cal_to_fake_profile, vcgt_to_cal
-from config import (fs_enc, get_display_profile, get_display_rects, getcfg,
-					geticon, get_verified_path, setcfg)
+from config import (fs_enc, get_argyll_display_number, get_display_profile,
+					get_display_rects, getcfg, geticon, get_verified_path,
+					setcfg)
 from log import safe_print
 from meta import name as appname
 from options import debug
@@ -920,25 +921,6 @@ class LUTFrame(wx.Frame):
 					self.gTRC.append([y, v])
 				elif i == 2:
 					self.bTRC.append([y, v])
-
-	def move_handler(self, event):
-		if not self.IsShownOnScreen():
-			return
-		display_no, geometry, client_area = self.get_display()
-		if display_no != self.display_no:
-			self.display_no = display_no
-			# Translate from wx display index to Argyll display index
-			geometry = "%i, %i, %ix%i" % tuple(geometry)
-			for i, display in enumerate(getcfg("displays").split(os.pathsep)):
-				if display.find("@ " + geometry) > -1:
-					if debug:
-						safe_print("[D] Found display %s at index %i" % 
-								   (geometry, i))
-					# Save Argyll display index to configuration
-					setcfg("display.number", i + 1)
-					self.load_lut(get_display_profile(i))
-					break
-		event.Skip()
 	
 	def reload_vcgt_handler(self, event):
 		cmd, args = self.worker.prepare_dispwin(True)
@@ -1416,15 +1398,15 @@ def main(profile=None):
 	app = LUTViewer(0)
 	app.frame.worker.enumerate_displays_and_ports(check_lut_access=False,
 												  enumerate_ports=False)
-	app.frame.Bind(wx.EVT_MOVE, app.frame.move_handler, app.frame)
-	app.frame.Show(True)
 	app.frame.update_controls()
 	if profile and profile.startswith("-"):
 		profile = None
 	if profile:
 		app.frame.drop_handler(profile)
 	else:
-		app.frame.load_lut(get_display_profile())
+		display_no = get_argyll_display_number(app.frame.get_display()[1])
+		app.frame.load_lut(get_display_profile(display_no))
+	app.frame.Show()
 	app.MainLoop()
 
 if __name__ == '__main__':

dispcalGUI/wxProfileInfo.py
@@ -7,9 +7,9 @@ import os
 import sys
 import tempfile
 
-from config import (defaults, fs_enc, get_data_path,
-					get_display_profile, getbitmap, getcfg, geticon, setcfg,
-					writecfg)
+from config import (defaults, fs_enc, get_argyll_display_number, get_data_path,
+					get_display_profile, get_display_rects, getbitmap, getcfg,
+					geticon, setcfg, writecfg)
 from meta import name as appname
 from ordereddict import OrderedDict
 from util_str import safe_unicode, universal_newlines, wrap
@@ -1010,6 +1010,9 @@ class ProfileInfoFrame(LUTFrame):
 		self.Bind(wx.EVT_MOVE, self.OnMove)
 		self.Bind(wx.EVT_SIZE, self.OnSize)
 		self.Bind(wx.EVT_SPLITTER_SASH_POS_CHANGING, self.OnSashPosChanging)
+		
+		self.display_no = -1
+		self.display_rects = get_display_rects()
 
 		children = self.GetAllChildren()
 
@@ -1410,8 +1413,9 @@ def main(profile=None):
 	lang.init()
 	lang.update_defaults()
 	app = ProfileInfoViewer(0)
-	app.frame.LoadProfile(profile or get_display_profile())
-	app.frame.Show(True)
+	display_no = get_argyll_display_number(app.frame.get_display()[1])
+	app.frame.LoadProfile(profile or get_display_profile(display_no))
+	app.frame.Show()
 	app.MainLoop()
 	writecfg()
 

commit 398d1ca3389b7fe93928f19d79c997eb38d27b9e

Fix typo introduced in r1737
git-svn-id: https://svn.code.sf.net/p/dispcalgui/code/trunk@1913 6ae35eb0-b8ff-4ca7-919d-0ec1eff389cd

dispcalGUI/wxMeasureFrame.py
@@ -470,7 +470,7 @@ class MeasureFrame(InvincibleFrame):
 			n = get_argyll_display_number(geometry)
 			if n is not None:
 				# Save Argyll display index to configuration
-				setcfg("display.number", i + 1)
+				setcfg("display.number", n + 1)
 
 	def size_handler(self, event):
 		if debug: safe_print("[D] measureframe_size_handler")